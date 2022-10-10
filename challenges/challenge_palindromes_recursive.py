985d64126caad23a10187a76e9d4fc6b24fe0829
Airam Toscano <airamlobato@gmail.com>
2022-10-10 15:51:58 -0400

    Feat: challenge_palindromes_recursive

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,12 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
+    if (word == ''):
+        return False
+    arrayWord = list(word)
+    if (arrayWord[low_index] != arrayWord[high_index]):
+        return False
+    if (low_index >= high_index):
+        return True
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+
+
+# print(is_palindrome_recursive('AGUA',0,3))
