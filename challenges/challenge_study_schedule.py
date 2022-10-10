d0119be69d7f96a656331aba53949d43e1f2473c
Airam Toscano <airamlobato@gmail.com>
2022-10-10 15:05:55 -0400

    Feat: challenge study schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,13 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
+    cont = 0
+    try:
+        for i in permanence_period:
+            if (int(i[0]) <= target_time <= int(i[1])):
+                cont += 1
+        return cont
+    except Exception:
+        return None
+
+
+# permanence_period = [(2, 2), (1, 2), (2, 2), (1, 5), (4, 5), (4, 5)]
+# print(study_schedule(permanence_period, ''))
