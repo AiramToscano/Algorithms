b0d3eeabaa57545911110bba67c1823de6186c03
Airam Toscano <airamlobato@gmail.com>
2022-10-11 18:13:06 -0400

    Feat: refactor study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -8,7 +8,3 @@ def study_schedule(permanence_period, target_time):
         return cont
     except Exception:
         return None
-
-
-# permanence_period = [(2, 2), (1, 2), (2, 2), (1, 5), (4, 5), (4, 5)]
-# print(study_schedule(permanence_period, ''))
