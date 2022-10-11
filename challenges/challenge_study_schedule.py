64bbb87df25e1800b7c31e6049b0086e99e75b90
Airam Toscano <airamlobato@gmail.com>
2022-10-11 17:15:42 -0400

    Feat: refactory study shedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,13 +1,16 @@
+
+
 def study_schedule(permanence_period, target_time):
     cont = 0
-    try:
-        for i in permanence_period:
-            if (int(i[0]) <= target_time <= int(i[1])):
-                cont += 1
-        return cont
-    except Exception:
+    if (target_time == '' or target_time is None):
         return None
+    for i in permanence_period:
+        if (type(i[0]) != int or type(i[1]) != int):
+            return None
+        if (i[0] <= target_time <= i[1]):
+            cont += 1
+    return cont
 
 
 # permanence_period = [(2, 2), (1, 2), (2, 2), (1, 5), (4, 5), (4, 5)]
-
+# print(study_schedule(permanence_period ,2))
