6ff71281f3496086240d7963a457ca8796aefc1d
Airam Toscano <airamlobato@gmail.com>
2022-10-12 17:10:14 -0400

    Feat: challenge_palindromes_iterative

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,17 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
+    if (word == ''):
+        return False
+    arrayWord = list(word)
+    tamanho = arrayWord.__len__()
+    tamanhoFinal = tamanho - 1
+    tamnahoInicial = 0
+    for i in arrayWord:
+        if (arrayWord[tamnahoInicial] != arrayWord[tamanhoFinal]):
+            return False
+        if (tamnahoInicial >= tamanhoFinal):
+            return True
+        tamnahoInicial += 1
+        tamanhoFinal -= 1
+
+
+# print(is_palindrome_iterative('AIRAM'))
