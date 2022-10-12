1310d402b3a36dfa5dca2754fc3162e7f8df1480
Airam Toscano <airamlobato@gmail.com>
2022-10-12 12:30:02 -0400

    Feat: find_the_duplicate

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,19 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
+    try:
+        return duplicate(nums)
+    except Exception:
+        return False
+
+
+def duplicate(nums):
+    maior = []
+    arrayOrd = sorted(nums)
+    for i in arrayOrd:
+        if (int(i) < 1):
+            return False
+        if (arrayOrd.count(i) > 1 and i not in maior):
+            maior.append(i)
+    return maior[0]
+
+
+# print(find_duplicate([1, 3, 4, 2, 2, 2]))
